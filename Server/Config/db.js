89f055585a669acf9858fed2decb57a9dda6d006
minli999 <presley.us@gmail.com>
2024-01-26 01:09:30 -0500

    adding remote database

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -4,6 +4,7 @@ exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 
 exports.LocalURI = "mongodb://127.0.0.1/store";
 exports.RemoteURI = process.env.RemoteURI;
 //localhost: connect to local database; remotehost: connect to cloud database
-exports.HostName = (process.env.RemoteURI) ? "remotehost" : "localhost";
+//export const HostName = (process.env.RemoteURI) ? "remotehost" : "localhost";
+exports.HostName = "remotehost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -2,5 +2,6 @@ export const LocalURI = "mongodb://127.0.0.1/store";
 export const RemoteURI = process.env.RemoteURI;
 
 //localhost: connect to local database; remotehost: connect to cloud database
-export const HostName = (process.env.RemoteURI) ? "remotehost" : "localhost";
+//export const HostName = (process.env.RemoteURI) ? "remotehost" : "localhost";
+export const HostName = "remotehost";
 export const Secret = "someSecret";
